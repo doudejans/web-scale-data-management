5b08c3407c77b02129f3ab1c30709388a1517b70
Martijn Comans <m.r.comans@student.tudelft.nl>
2020-05-05 20:27:10 +0200

    Add setup for postgres

--- order-service/database/postgres.py
+++ order-service/database/postgres.py
@@ -15,16 +15,23 @@ class PostgresDB(Database):
                                            user=connection_config["user"],
                                            database=connection_config["database"],
                                            password=connection_config["password"])
-        # TODO: Add specific connection code, if needed.
         if setup:
             self.__setup_database(config)
 
     def __setup_database(self, config):
-        # connection.execute(f"""
-        #   CREATE TABLE name ....
-        # """")
-        # TODO: Add logic to setup the db and tables.
-        pass
+        cursor = self.connection.cursor()
+        cursor.execute('''
+        CREATE TABLE orders (order_id uuid CONSTRAINT orders_pk PRIMARY KEY, user_id uuid NOT NULL);
+        CREATE INDEX orders_user_id_index ON orders (user_id);
+        ''')
+
+        cursor.execute('''
+        CREATE TABLE order_items (
+        order_id uuid, item_id uuid, amount int, CONSTRAINT order_items_pk PRIMARY KEY (order_id, item_id)
+        );
+        ''')
+        self.connection.commit()
+        cursor.close()
 
     def retrieve_version(self):
         # This is an example for a query. The same query, with the same function
