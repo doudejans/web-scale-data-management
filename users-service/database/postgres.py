5ee1cc8b2532b66a94cdd18213fd8f5d88aa5fc3
Michael Leichtfried <m.leichtfried@student.tudelft.nl>
2020-05-05 22:12:50 +0200

    Add first impl of users-service for PG

--- users-service/database/postgres.py
+++ users-service/database/postgres.py
@@ -1,4 +1,8 @@
+import uuid
+
 import psycopg2
+import psycopg2.extras
+
 from database.database import Database
 
 
@@ -15,24 +19,72 @@ class PostgresDB(Database):
                                            user=connection_config["user"],
                                            database=connection_config["database"],
                                            password=connection_config["password"])
-        # TODO: Add specific connection code, if needed.
+        psycopg2.extras.register_uuid()
         if setup:
             self.__setup_database(config)
 
     def __setup_database(self, config):
-        # connection.execute(f"""
-        #   CREATE TABLE name ....
-        # """")
-        # TODO: Add logic to setup the db and tables.
+        with self.connection.cursor() as cur:
+            cur.execute(f'''
+            CREATE TABLE users (
+                user_id uuid,
+                credit integer
+            );
+            ''')
         pass
 
     def retrieve_version(self):
         # This is an example for a query. The same query, with the same function
         # name, parameters and return type, should be implemented for the other
         # database.
-        curr = self.connection.cursor()
-        curr.execute("""
-            SELECT version()
-        """)
-        result = curr.fetchone()
-        return result
+        with self.connection.cursor() as cur:
+            cur.execute("""
+                SELECT version()
+            """)
+            result = cur.fetchone()
+            return result
+
+    def create_user(self):
+        user_id = uuid.uuid4()
+        with self.connection.cursor() as cur:
+            cur.execute('''
+            INSERT INTO users (user_id, credit)
+            VALUES (%s, 0);
+            ''', (user_id,))
+            return str(user_id)
+
+    def remove_user(self, user_id):
+        with self.connection.cursor() as cur:
+            cur.execute('''
+            DELETE FROM users
+            WHERE user_id = %s;
+            ''', (user_id,))
+            return cur.rowcount == 1
+
+    def get_credit(self, user_id):
+        with self.connection.cursor() as cur:
+            cur.execute('''
+            SELECT credit FROM users
+            WHERE user_id = %s;
+            ''', (user_id,))
+            res = cur.fetchone()
+            return None if res is None else res[0]
+
+    def credit_add(self, user_id, amount):
+        with self.connection.cursor() as cur:
+            cur.execute('''
+            UPDATE users
+            SET credit = credit + %s
+            WHERE user_id = %s;
+            ''', (amount, user_id))
+            return cur.rowcount == 1
+
+    def credit_subtract(self, user_id, amount):
+        with self.connection.cursor() as cur:
+            cur.execute('''
+            UPDATE users
+            SET credit = credit - %s
+            WHERE user_id = %s
+            AND credit >= %s;
+            ''', (amount, user_id, amount))
+            return cur.rowcount == 1
