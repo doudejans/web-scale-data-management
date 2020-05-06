99ffdb9a6f6df3dc14a456914a8bc272e500528d
Nick Belzer <me@nickbelzer.me>
2020-05-06 17:54:42 +0200

    Read config from env variables

--- template-module/setup.py
+++ template-module/setup.py
@@ -1,23 +1,24 @@
 import routes
-from config import read_config
+from config import retrieve_config
 from database.database import Database
 from database.postgres import PostgresDB
 from database.cassandra import CassandraDB
 
+APPLICATION_NAME = "microservice"
 
-def setup_app(config_file: str = "config/config.yaml"):
+
+def setup_app():
     """
     Application setup code based on configuration file.
 
     This logic is located here as it is shared between the development and
     wsgi mode.
     """
-    config = read_config(config_file)
+    config = retrieve_config(APPLICATION_NAME)
 
-    application_name = config['name']
     database_config = config['database']
 
-    print(f"Starting {application_name}")
+    print(f"Starting {APPLICATION_NAME}")
     print(f" - Using a {database_config['type']} database.")
     db: Database = PostgresDB() if database_config['type'] == "postgres" \
         else CassandraDB()
