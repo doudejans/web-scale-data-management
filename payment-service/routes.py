ba92974c6d89c23587be9dab3e6689aa503f2a53
Nick Belzer <me@nickbelzer.me>
2020-05-12 11:55:10 +0200

    Incorporate external services in payment logic

--- payment-service/routes.py
+++ payment-service/routes.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, make_response
 from http import HTTPStatus
 from database.database import Database
+from external_services import CouldNotSubtractCredit, \
+    CouldNotRetrieveOrderCost, retrieve_order_cost, subtract_user_credit
+
 
 # This is the main file of the service, handling with the different routes
 # the microservice exposes.
@@ -10,6 +13,7 @@ from database.database import Database
 # based on the config given to the application.
 def create_app(db: Database):
     service = Flask(__name__)
+
     # The db variable will be set by app.py to either cassandra or postgres
     # depending on flags used when starting the application. You can assume
 
@@ -19,8 +23,28 @@ def create_app(db: Database):
 
     @service.route('/pay/<uuid:user_id>/<uuid:order_id>', methods=["POST"])
     def complete_payment(user_id, order_id):
-        db.set_payment_status(order_id, "PAID")
-        return make_response(jsonify(), HTTPStatus.CREATED)
+        try:
+            order_cost = retrieve_order_cost(order_id)
+            subtract_user_credit(user_id, order_cost)
+            try:
+                db.set_payment_status(order_id, "PAID")
+            except:
+                # If we cannot store the entry in the db, revert.
+                # TODO: Figure out how te revert.
+                pass
+            return make_response(jsonify(), HTTPStatus.CREATED)
+        except CouldNotRetrieveOrderCost as e:
+            # If we cannot find the order_cost.
+            # TODO: Determine correct HTTPStatus
+            return make_response(jsonify({
+                "error": "Could not find order"
+            }), HTTPStatus.BAD_REQUEST)
+        except CouldNotSubtractCredit as e:
+            # If we cannot subtract the amount
+            # TODO: Determine correct HTTPStatus
+            return make_response(jsonify({
+                "error": "Not enough credits on account"
+            }), HTTPStatus.BAD_REQUEST)
 
     @service.route('/cancel/<uuid:user_id>/<uuid:order_id>', methods=["POST"])
     def cancel_payment(user_id, order_id):
@@ -39,4 +63,3 @@ def create_app(db: Database):
             return make_response(jsonify(), HTTPStatus.NOT_FOUND)
 
     return service
-
