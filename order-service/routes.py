e0cd80681949e208085e86ea087653a51360a074
Martijn Comans <m.r.comans@student.tudelft.nl>
2020-06-03 10:35:21 +0200

    Remove status field in order service responses

--- order-service/routes.py
+++ order-service/routes.py
@@ -21,41 +21,41 @@ def create_app(db: Database):
     @service.route('/remove/<uuid:order_id>', methods=['DELETE'])
     def remove_order(order_id: UUID):
         db.delete_order(order_id)
-        return jsonify({'status': 200, 'message': 'success'})
+        return jsonify({'message': 'success'})
 
     @service.route('/find/<uuid:order_id>')
     def find_order(order_id: UUID):
         order = db.get_order(order_id)
         if order is None:
-            return jsonify({'status': 404, 'message': 'Order not found'}), 404
+            return jsonify({'message': 'Order not found'}), 404
 
         # TODO: contact payment service for status
 
-        return jsonify({'status': 200, 'order': order})
+        return jsonify({'order': order})
 
     @service.route('/addItem/<uuid:order_id>/<uuid:item_id>', methods=['POST'])
     def add_item_to_order(order_id: UUID, item_id: UUID):
         found_order = db.add_item_to_order(order_id, item_id)
 
         if found_order:
-            return jsonify({'status': 200, 'message': 'success'})
+            return jsonify({'message': 'success'})
         else:
-            return jsonify({'status': 404, 'message': 'Order not found'}), 404
+            return jsonify({'message': 'Order not found'}), 404
 
     @service.route('/removeItem/<uuid:order_id>/<uuid:item_id>', methods=['DELETE'])
     def remove_item_from_order(order_id: UUID, item_id: UUID):
         found_order = db.remove_item_from_order(order_id, item_id)
 
         if found_order:
-            return jsonify({'status': 200, 'message': 'success'})
+            return jsonify({'message': 'success'})
         else:
-            return jsonify({'status': 404, 'message': 'Order not found'}), 404
+            return jsonify({'message': 'Order not found'}), 404
 
     @service.route('/checkout/<uuid:order_id>', methods=['POST'])
     def checkout_order(order_id: UUID):
         # TODO: Contact stock service
         # TODO: Contact payment service
-        return jsonify({'status': 200, 'message': 'Not implemented yet'})
+        return jsonify({'message': 'Not implemented yet'})
 
     return service
 
